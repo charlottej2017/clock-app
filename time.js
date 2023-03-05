fe870b2371cf04147060efd95c1c40c19b31e95c
charlottej2017 <Charlottejones74@gmail.com>
2023-03-05 14:03:44 -0500

    converted time to 12 hr and updated background color

--- time.js
+++ time.js
@@ -11,4 +11,34 @@ setInterval(() => {
   hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
   mn.style.transform = `rotateZ(${mm}deg)`;
   sc.style.transform = `rotateZ(${ss}deg)`;
+
+  // Digital Clock
+
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+  let ampm = document.getElementById("ampm");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  let am = h >= 12 ? "PM" : "AM";
+
+  // convert 24 hr clock to 12 hr clock
+
+  if (h > 12) {
+    h = h - 12;
+  }
+
+  // add zero before signle digit number
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
 });
